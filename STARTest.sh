286454516f42e7432e87c6a63e81d151c0fb16d3
gugascheckerl <155962949+gugascheckerl@users.noreply.github.com>
2024-01-09 12:50:43 +0100

    Add files via upload

--- STARTest.sh
+++ STARTest.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-#SBASH --name=starTest
-#SBASH --nodes=1
-#SBASH --cpus-per-task=16
-#SBASH --mem=4G
-#SBASH --time=10:00:00
-#SBASH --partition=basic
-#SBASH --output=/lisc/scratch/course/2023w300106/tillmann/star.log
-#SBASH --error=/lisc/scratch/course/2023w300106/tillmann/star.err
-#SBASH --mail-type=END
-#SBASH --mail-user=a00545087@unet.univie.ac.at
+
+#SBATCH --job-name=starTest
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16G
+#SBATCH --time=10:00:00
+#SBATCH --partition=basic
+#SBATCH --output=/lisc/scratch/course/2023w300106/tillmann/star.log
+#SBATCH --error=/lisc/scratch/course/2023w300106/tillmann/star.err
+#SBATCH --mail-type=END
+#SBATCH --mail-user=a00545087@unet.univie.ac.at
+
+### ENVIRONMENT
 module load star
 module list
 
+### SETTING UP DIRECTORIES
+# cd into my own directory
 cd /lisc/scratch/course/2023w300106/tillmann
 mkdir ex5
 cd ex4
 
-STAR --runThreadN 16 --genomeDir /lisc/scratch/course/2023w300106/tillmann/ex4/ncbi_dataset/data/GCF_932526225.1/jaNemVect1.1_STAR readFilesIn /lisc/scratch/course/2023w300106/tillmann/ex3/SRR24348397/SRR24348397_1.fastq /lisc/scratch/course/2023w300106/tillmann/ex3/SRR24348397/SRR24348397_2.fastq --outSAMtype BAM SortedbyCoordinate --twopassMode Basic --outSAMstrandFile intronMotif --outFileNamePrefix SRR24348397
+### RUNNING THE COMMAND
+STAR --runThreadN 16 --genomeDir /lisc/scratch/course/2023w300106/jmontenegro/ex2/ncbi_dataset/data/GCF_932526225.1/jaNemVect1.1_STAR --readFilesIn /lisc/scratch/course/2023w300106/tillmann/ex3/SRR24348397/SRR24348397_1.fastq /lisc/scratch/course/2023w300106/tillmann/ex3/SRR24348397/SRR24348397_2.fastq --outSAMtype BAM SortedByCoordinate --twopassMode Basic --outSAMstrandField intronMotif --outFileNamePrefix SRR24348397
+
+
 
